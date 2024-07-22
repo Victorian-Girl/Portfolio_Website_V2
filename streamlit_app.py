fa44f22b6872f5716afe35a7dd1ba63231d271ae
Melissa <metalomax@gmail.com>
2024-07-22 11:45:37 -0400

    putting text at botton of the side bar

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,11 @@ st.sidebar.write("GitHub:  https://github.com/Victorian-Girl")
 st.sidebar.write("StackOverFlow: https://stackoverflow.com/users/15284428/m%c3%a9lissa-ch")
 # Adding a text
 st.logo("assets/logo4.jpg")                                                                                             # 24 height, max width = 240 px or 10:1 ratio
-st.sidebar.text("Made with l'❤️ by Mélissa")
+# st.sidebar.text("Made with l'❤️ by Mélissa")
+
+# Afficher du texte en bas du panneau latéral
+st.write(" ")
+st.sidebar.write('<div style="position: fixed; bottom: 10px; width: 95%;">Made with ❤️ by Mélissa</div>', unsafe_allow_html=True)
 
 
 # --- RUN NAVIGATION ---
