bc012461ceb5f94062b7e4783031062f9a4ece67
Melissa <metalomax@gmail.com>
2024-07-22 21:12:30 -0400

    adding logo image to link's and some html code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
+import base64
+
 import streamlit as st
 
 
@@ -13,7 +15,6 @@ current_page = query_params.get("page", "")
 # Based on the current page, initialize the chatbot
 if current_page == "about_me":
     initialize_chatbot()
-
 
 # --- PAGE SETUP ---
 
@@ -39,7 +40,6 @@ about_page = st.Page(
     default=True,
 )
 
-
 # --- NAVIGATION SETUP [WITH SECTIONS]---
 pg = st.navigation(
     {
@@ -49,21 +49,75 @@ pg = st.navigation(
     }
 )
 
-
 # --- SHARED ON ALL PAGES ---
-st.sidebar.write("Professional Website: https://melissa.christiaenssens.svija.site/")
-st.sidebar.write("Artis Website: https://mcrist.artiste.svija.site/")
-st.sidebar.write("LinkedIn: https://www.linkedin.com/in/melissa-ch")
-st.sidebar.write("GitHub:  https://github.com/Victorian-Girl")
-st.sidebar.write("StackOverFlow: https://stackoverflow.com/users/15284428/m%c3%a9lissa-ch")
-# Adding a text
+image_path = "assets/website1.png"                                                                                      # path to the image
+image2_path = "assets/linkedin.png"
+image3_path = "assets/git.png"
+image4_path = "assets/stackoverflow.png"
+image5_path = "assets/youtube.png"
+
+st.sidebar.markdown(                                                                                                    # Adding a markdown content to the sidebar panel
+    """
+    <h2>Professional Website:
+    <a href="https://melissa.christiaenssens.svija.site/">
+        <img src="data:image/png;base64,{}" alt="Logo" style="width:40px;">
+    </a></h2>
+    """.format(base64.b64encode(open(image_path, "rb").read()).decode()), unsafe_allow_html=True
+)
+
+st.sidebar.markdown(
+    """
+    <h2>Artist Website:
+    <a href="https://mcrist.artiste.svija.site/">
+        <img src="data:image/png;base64,{}" alt="Logo" style="width:40px;">
+    </a></h2>
+    """.format(base64.b64encode(open(image_path, "rb").read()).decode()), unsafe_allow_html=True
+)
+
+st.sidebar.markdown(
+    """
+    <h2>LinkedIn:
+    <a href="https://www.linkedin.com/in/melissa-ch">
+        <img src="data:image/png;base64,{}" alt="Logo" style="width:30px;">
+    </a></h2>
+    """.format(base64.b64encode(open(image2_path, "rb").read()).decode()), unsafe_allow_html=True
+)
+
+st.sidebar.markdown(
+    """
+    <h2>GitHub:
+    <a href="https://github.com/Victorian-Girl">
+        <img src="data:image/png;base64,{}" alt="Logo" style="width:30px;">
+    </a></h2>
+    """.format(base64.b64encode(open(image3_path, "rb").read()).decode()), unsafe_allow_html=True
+)
+
+st.sidebar.markdown(
+    """
+    <h2>Stackoverflow:
+    <a href="https://stackoverflow.com/users/15284428/m%c3%a9lissa-ch">
+        <img src="data:image/png;base64,{}" alt="Logo" style="width:30px;">
+    </a></h2>
+    """.format(base64.b64encode(open(image4_path, "rb").read()).decode()), unsafe_allow_html=True
+)
+
+st.sidebar.markdown(
+    """
+    <h2>YouTube:
+    <a href="https://www.youtube.com/@M%C3%A9lissaCh-i6p">
+        <img src="data:image/png;base64,{}" alt="Logo" style="width:30px;">
+    </a></h2>
+    """.format(base64.b64encode(open(image5_path, "rb").read()).decode()), unsafe_allow_html=True
+)
+
+
+# --- LOGO and FOOTER ---
 st.logo("assets/logo4.jpg")                                                                                             # 24 height, max width = 240 px or 10:1 ratio
-# st.sidebar.text("Made with l'❤️ by Mélissa")
 
-# Afficher du texte en bas du panneau latéral
+
 st.write(" ")
-st.sidebar.write('<div style="position: fixed; bottom: 10px; width: 95%;">Made with ❤️ by Mélissa</div>', unsafe_allow_html=True)
-
+st.sidebar.write('<div style="position: fixed; bottom: 10px; width: 95%;">Made with ❤️ by Mélissa</div>',               # Adding a footer
+                 unsafe_allow_html=True)
 
 # --- RUN NAVIGATION ---
 pg.run()
